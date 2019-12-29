43218b4c35cf5f249961da5696edfaac12fca585
Alibn22 <a.baghchehban1@gmail.com>
2019-12-29 13:13:08 +0330

    add profile schema

--- src/schemas/Profile.ts
+++ src/schemas/Profile.ts
@@ -1,6 +1,23 @@
 import { Typegoose, prop, Ref } from "typegoose";
 
 export class Profile extends Typegoose {
+    @prop()
+    email: string
+
+    @prop()
+    name: string
+
+    @prop()
+    phoneNo: string
+
+    @prop()
+    nationalCode: string
+
+    @prop()
+    address: number
+
+    @prop()
+    postalCode: string
 }
 
 export default Profile
